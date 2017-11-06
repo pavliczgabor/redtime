b9e684893692cf760d7f1f1192ada75f9b76d270
Tamássy Urmás Asurm12 <pumt99@gmail.com>
2017-11-06 11:59:32 +0100

    Fejlesztés #26 Fejlesztések koordinálása Controller átnevezés referencia-hibák kerülése érdekében Backlog item #20,#22: Szűrőfeltételek bekötése, Backend elkészítése és bekötése a megjelenítéssel.
    Signed-off-by: Asurm <pumt99@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 get 'redtime', to: 'redtime_main#index'
+get 'timereport', to: 'redtime_main#time_report'
 get 'pages/:page' => 'page#show'
