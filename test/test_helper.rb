ca1a5093785bc63f5e1176f340d7818b5b3ced49
Pávlicz Gábor <pavlicz.gabor@tigra.hu>
2017-11-20 23:47:58 +0100

    Revert "Plugin teszt 1."
    This reverts commit 06fabea6d144c8b6657cd8d1091e7aa8f54ebfe7.

--- test/test_helper.rb
+++
@@ -1,5 +0,0 @@
-$VERBOSE = nil
-
-require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
-
-ActiveRecord::FixtureSet.create_fixtures(File.dirname(__FILE__) + '/fixtures/', [:projects])
