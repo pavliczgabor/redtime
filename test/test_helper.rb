e4e401e9629600edddb72bc5a988fdcfeb1901d2
Gabor Pavlicz <pavlicz.gabor@tigra.hu>
2017-11-21 15:46:29 +0100

    Funkciónális tesztek

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,4 @@ $VERBOSE = nil
 
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 
-ActiveRecord::FixtureSet.create_fixtures(File.dirname(__FILE__) + '/fixtures/', [:projects])
+#ActiveRecord::FixtureSet.create_fixtures(File.dirname(__FILE__) + '/fixtures/', [:projects])
--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -5,8 +5,10 @@ class ProjectTest < ActiveSupport::TestCase
   fixtures :projects
 
   test "Loaded test project" do
-    assert_equal 1, Project.count
-    assert_equal "Teszt projekt", Project.first.name
+    #assert_equal 1, Project.count
+    #assert_equal "Teszt projekt", Project.first.name
+    assert_equal 6, Project.count
+    assert_equal "eCookbook", Project.first.name    
   end
 
 end
