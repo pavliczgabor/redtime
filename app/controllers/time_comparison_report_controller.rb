a2edf639359eb489eeb087bd551e0da858215164
Világos Emil <vemil77@gmail.com>
2017-11-24 19:55:06 +0100

    #36 Filterek hozzadasa

--- app/controllers/time_comparison_report_controller.rb
+++ app/controllers/time_comparison_report_controller.rb
@@ -1,8 +1,42 @@
 class TimeComparisonReportController < ApplicationController
   def filter
+
+    @projectAll = Project.all
+    @projectAll_selection=[]
+
+    @projectAll.each do |projects|
+      @projectAll_selection<<[projects.name, projects.id]
+    end
+
     @project = Project.find(params[:id])
+    @versions_selection=[]
+
+    @project.versions.each do |version|
+      @versions_selection<<[version.name,version.id]
+    end
+
+    @tracker = Tracker.all
+    @trackers_selection=[]
+
+    @tracker.each do |tracker|
+      @trackers_selection<<[tracker.name,tracker.id]
+    end
+
+
   end
+
   def report
     @project = Project.find(params[:id])
+    datas=Array.new
+    if params.key?(:versions)
+      sprints=Version.find(params[:versions])
+    else
+      sprints=@project.versions
+    end
+    if params.key?(:users)
+      users=User.find(params[:users])
+    else
+      users=@project.users.to_enum
+    end
   end
 end
