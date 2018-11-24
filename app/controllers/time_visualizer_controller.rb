f6bfa72faee9248512637c6d7cf5b7d26f42b187
BotondToth <tothboti@gmail.com>
2018-11-24 09:54:14 +0100

    refs #77 - Kód refaktor

--- app/controllers/time_visualizer_controller.rb
+++ app/controllers/time_visualizer_controller.rb
@@ -3,45 +3,66 @@ class TimeVisualizerController < ApplicationController
   def index
     @project = Project.find(params[:id])
     @sprints_selection = []
+    get_sprints_selection()
+
+
+    @trackers_selection = []
+    get_trackers_selection()
+
+    @sprints = get_sprints()
+    @trackers = get_trackers()
+
+    calculate_graph()
+  end
+
+  #relevans sprintek kivalasztasa
+  def get_sprints
+    if params.key?(:sprints)
+      @sprints = Version.where(id: params[:sprints]).order(:created_on)
+    else
+      @sprints = @project.versions.order(:created_on)
+    end
+  end
+
+ #szukseges trackerek kivalasztasa
+  def get_trackers
+    if (params.key?(:trackers))
+      @trackers = Tracker.find(params[:trackers])
+    else
+      @trackers = @project.trackers
+    end
+  end
+
+  #sprintek nevenek es id-jenek kiszurese
+  def get_sprints_selection
     @project.versions.each do |version|
       @sprints_selection << [version.name, version.id]
     end
     @sprints_selection.sort
+  end
 
-    @trackers_selection = []
+  #trackerek nevenek es id-jenek kiszurese
+  def get_trackers_selection
     @project.trackers.each do |tracker|
       @trackers_selection << [tracker.name, tracker.id]
     end
+  end
 
-    #Szűrés az adott paraméterekkel.
+  # @dataItems feltoltese valós adatokkal
+  def calculate_graph
     @dataItems = []
-    if params.key?(:sprints)
-      sprints = Version.where(id: params[:sprints]).order(:created_on)
-    else
-      sprints = @project.versions.order(:created_on)
-    end
 
-    if (params.key?(:trackers))
-      trackers = Tracker.find(params[:trackers])
-    else
-      trackers = @project.trackers
-    end
-    #megadott sprint(ek) és tracker(ek)0
-    sprint_names = sprints.map {|f| f.name}
-    tracker_names = trackers.map {|f| f.name}
     #TODO: dokumentacio ir szurest a "projekthez rendelt megadott szerepkoru felhasznalora" is
-
-    # @dataItems feltoltese valós adatokkal
-    issues = Issue.where(fixed_version: sprints, tracker: trackers)
+    issues = Issue.where(fixed_version: @sprints, tracker: @trackers)
     time_entries = TimeEntry.where(issue: issues)
     journals = Journal.where(journalized: issues)
-    journal_details = JournalDetail.where(journal: journals) if !journals.empty? 
+    journal_details = JournalDetail.where(journal: journals) if !journals.empty?
     work_hour_per_week = 0
     @project.members.each do |member|
       work_hour_per_week += member.user.custom_field_value(UserCustomField.find_by(name: "Munkaóra/Hét").id).to_i
     end
-    start_date = sprints.first.created_on.to_date
-    end_date = sprints.last.effective_date.to_date
+    start_date = @sprints.first.created_on.to_date
+    end_date = @sprints.last.effective_date.to_date
     issue_hours = 0
     added_issues = []
     added_time_entires = []
@@ -57,7 +78,6 @@ class TimeVisualizerController < ApplicationController
             if !added_time_entires.include?(te) && te.spent_on <= date && !added_issues.find {|is| is[:issue] == issue}[:finished]
               added_time_entires << te
               issue_hours -= te.hours
-              puts "kiscica"
               puts added_issues.find {|is| is[:issue] == issue}[:time_left].class
               added_issues.find {|is| is[:issue] == issue}[:time_left] -= te.hours
             end
@@ -76,14 +96,6 @@ class TimeVisualizerController < ApplicationController
       end
       @dataItems << StatItem.new(1, "valami", {date => {:remaining_work_hour => ((work_days / 7.0) * work_hour_per_week).to_i, :remaining_issue_hour => issue_hours}})
     end
-    # @dataItems feltoltese dummy adatokkal
-
-    #@dataItems << StatItem.new(sprints.map{|f| f.name},trackers.map{|f| f.name}, {Date.new(2018,5, 20) => {:remaining_work_hour => 220, :remaining_issue_hour => 150 }})
-    # @dataItems << StatItem.new(0.1,"Feladat", {Date.new(2018,5, 20) => {:remaining_work_hour => 220, :remaining_issue_hour => 150 }})
-    # @dataItems << StatItem.new(0.1,"Hiba", {Date.new(2018,5, 21) => {:remaining_work_hour => 180, :remaining_issue_hour => 150 }})
-    # @dataItems << StatItem.new(0.1,"Feladat", {Date.new(2018,5, 22) => {:remaining_work_hour => 100, :remaining_issue_hour => 90 }})
-    # @dataItems << StatItem.new(0.1,"Feladat", {Date.new(2018,5, 25) => {:remaining_work_hour => 50, :remaining_issue_hour => 40 }})
-    # @dataItems << StatItem.new(0.1,"Feladat", {Date.new(2018,5, 28) => {:remaining_work_hour => 30, :remaining_issue_hour => 20 }})
   end
 
 end
